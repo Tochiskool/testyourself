d9278e59cb040a8ff356be475212b8b960841e15
Tochiskool <mrchristian79@yahoo.com>
2023-08-26 22:09:34 +0200

    Instructions on what should have if player does not enter name and landing page updated

--- SCRIPT/load.js
+++ SCRIPT/load.js
@@ -1,14 +1,25 @@
 
-var nameEl, btn;
+var nameEl, btn, hideMe;
 
-const init = () => {
-    nameEl = document.querySelector('#nameEl');  
+function init() {
+    nameEl = document.querySelector('#nameEl'); 
+    hideMe = document.querySelector('#hideMe');
+    btn = document.querySelector('.btn');
 }
 
-const startGame = () => {
+function startGame(){
     console.log('Start now')
-    window.open("./HTML/question.html")
-    window.location.href = "./HTML/question.html";
+    
+    if (nameEl.value === "") {  
+        console.log('Hid me')
+        btn.style.display = "none";
+        return hideMe.textContent = " Please enter a valid name";
+    } else {
+        console.log("Open page")
+        window.open('./HTML/question.html');
+    //    window.location.href = "./HTML/question.html" 
+    }
+
  }
 
 document.querySelector('.btn').addEventListener('click', startGame, false)
